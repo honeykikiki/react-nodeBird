b3983a14ca60eecd22b18d4e0a6598cb993c91d2
honeykikiki <tjdwls75666@naver.com>
2021-09-13 19:51:03 +0900

    commit

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -25,12 +27,18 @@ db.sequelize
   .catch(console.error);
 passportConfig();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
 
 // cors문제 해결
 app.use(
   cors({
-    origin: 'http://localhost:3060', // 허용하는 페이지 주소
+    origin: ['http://localhost:3060', 'nodebird.com'], // 허용하는 페이지 주소
     credentials: true, // 쿠키를 전해줄떄
   }),
 );
